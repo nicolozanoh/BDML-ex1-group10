466670b2c2dee9d55d667a2ed65c2cffc6f67288
nicolozanoh <98645826+nicolozanoh@users.noreply.github.com>
2025-01-22 19:27:17 -0500

    Update impute1.R

--- impute1.R
+++ impute1.R
@@ -1,3 +1,24 @@
 # Include here your code for your first chosen imputation method
+# install pacman
+if(!require(pacman)) install.packages("pacman") ; require(pacman)
+
+# require/install packages on this session
+
+p_load(rio, # import/export data
+       tidyverse, # tidy-data
+       skimr, # summary data
+       visdat, # visualizing missing data
+       corrplot, # Correlation Plots 
+       stargazer) # tables/output to TEX. 
+        
+
+## load data
+df <- import("https://github.com/ignaciomsarmiento/datasets/blob/main/GEIH_sample1.Rds?raw=true")
+
+db <- as_tibble(df) ## from dataframe to tibble
+
+
+
+
 db <- db  %>%
   mutate(ingtot = ifelse(is.na(ingtot) == TRUE, median(db$ingtot, na.rm = TRUE) , ingtot))
